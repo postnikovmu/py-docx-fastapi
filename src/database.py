5ea797fd663e60d7eb7fa8183bae029778d4d837
Mikhail Postnikov <postnikovmu@mail.ru>
2025-03-09 02:37:43 +0300

    #3 add sqla pool

--- src/database.py
+++ src/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+psycopg://py_docx_fastapi:py_docx_fastapi@py_docx_fastapi_postgres:5432/py_docx_fastapi"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+# Create an async engine with connection pooling
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_size=10,          # Set the size of the pool
+    max_overflow=5         # Allow up to 5 additional connections beyond pool_size
+)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
